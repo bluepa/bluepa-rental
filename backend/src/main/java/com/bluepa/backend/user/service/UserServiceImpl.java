864340bceaf1c515059db50588b50c60c169710f
voiciphil <voiciphil@gmail.com>
2021-03-08 21:02:59 +0900

    [BE]add: 회원가입 서비스 구현

--- backend/src/main/java/com/bluepa/backend/user/service/UserServiceImpl.java
+++ backend/src/main/java/com/bluepa/backend/user/service/UserServiceImpl.java
@@ -1,13 +1,29 @@
 package com.bluepa.backend.user.service;
 
+import com.bluepa.backend.user.domain.User;
+import com.bluepa.backend.user.domain.UserRepository;
 import com.bluepa.backend.user.dto.SignUpRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public Long register(SignUpRequest signUpRequest) {
-        return null;
+        User user = User.builder()
+            .email(signUpRequest.getEmail())
+            .password(passwordEncoder.encode(signUpRequest.getPassword()))
+            .nickname(signUpRequest.getNickname())
+            .build();
+
+        User result = userRepository.save(user);
+
+        return result.getId();
     }
 }
