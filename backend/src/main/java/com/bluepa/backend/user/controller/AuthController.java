29cbe6afbf185777850af91d53182f1e7506f47a
voiciphil <voiciphil@gmail.com>
2021-03-12 14:55:27 +0900

    [BE]add: 로그인 API 추가

--- backend/src/main/java/com/bluepa/backend/user/controller/AuthController.java
+++ backend/src/main/java/com/bluepa/backend/user/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.bluepa.backend.user.controller;
 
+import com.bluepa.backend.global.security.JwtProperties;
+import com.bluepa.backend.user.dto.SignInRequest;
 import com.bluepa.backend.user.dto.SignUpRequest;
 import com.bluepa.backend.user.service.UserService;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -23,5 +26,13 @@ public class AuthController {
     public ResponseEntity<?> register(@RequestBody SignUpRequest request) {
         Long userId = userService.register(request);
         return ResponseEntity.created(URI.create("/api/users/" + userId)).build();
+    }
+
+    @PostMapping("/sign-in")
+    public ResponseEntity<?> signIn(@RequestBody SignInRequest request) {
+        String token = userService.signIn(request);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(JwtProperties.HEADER, JwtProperties.TYPE + " " + token);
+        return ResponseEntity.ok().headers(headers).build();
     }
 }
