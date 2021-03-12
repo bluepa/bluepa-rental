b9ba5d42efae8e6e55d0561a3452ac4bc455357d
zxcv859500 <zxcv859500@naver.com>
2021-03-12 12:35:30 +0900

    [BE] Test: Post Service Test

---
+++ backend/src/test/java/com/bluepa/backend/post/service/PostServiceTest.java
@@ -0,0 +1,31 @@
+package com.bluepa.backend.post.service;
+
+import com.bluepa.backend.post.domain.Post;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.elasticsearch.core.geo.GeoJsonPoint;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class PostServiceTest {
+
+  @Autowired PostService postService;
+
+  @Test
+  void write() {
+    Post post = new Post();
+    post.setTitle("test Title");
+    post.setAuthor("test Author");
+    post.setContent("set Content");
+    post.setLocation(GeoJsonPoint.of(100, 100));
+
+    String saveId = postService.write(post);
+
+    Post findPost = postService.findOne(saveId).get();
+    assertThat(post).isEqualTo(findPost);
+  }
+}
