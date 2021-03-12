0e4bf2e614f0d7659e4522b9749e25621e170218
zxcv859500 <zxcv859500@naver.com>
2021-03-12 12:03:57 +0900

    [BE] fix: delete save method

--- backend/src/main/java/com/bluepa/backend/post/repository/PostRepository.java
+++ backend/src/main/java/com/bluepa/backend/post/repository/PostRepository.java
@@ -5,6 +5,5 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, String> {
-  <S extends Post> S save(Post post);
   Optional<Post> findById(String id);
 }
