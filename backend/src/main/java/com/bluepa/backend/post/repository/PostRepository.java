657e2537a7d0fb67136f549e43247586a3ca7e28
zxcv859500 <zxcv859500@naver.com>
2021-03-12 11:42:47 +0900

    [BE] add: auto generate id

--- backend/src/main/java/com/bluepa/backend/post/repository/PostRepository.java
+++ backend/src/main/java/com/bluepa/backend/post/repository/PostRepository.java
@@ -4,7 +4,7 @@ import com.bluepa.backend.post.domain.Post;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Integer> {
+public interface PostRepository extends JpaRepository<Post, String> {
   <S extends Post> S save(Post post);
-  Optional<Post> findById(int id);
+  Optional<Post> findById(String id);
 }
