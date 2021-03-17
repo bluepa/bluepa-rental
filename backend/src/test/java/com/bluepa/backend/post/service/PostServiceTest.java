78584de47e40728c8648d2dc26e49dc0ad6a775f
zxcv859500 <zxcv859500@naver.com>
2021-03-17 22:12:36 +0900

    [BE] add: mock

--- backend/src/test/java/com/bluepa/backend/post/service/PostServiceTest.java
+++ backend/src/test/java/com/bluepa/backend/post/service/PostServiceTest.java
@@ -1,31 +1,57 @@
 package com.bluepa.backend.post.service;
 
+import static org.mockito.ArgumentMatchers.any;
+
 import com.bluepa.backend.post.domain.Post;
+import com.bluepa.backend.post.repository.PostRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.elasticsearch.core.geo.GeoJsonPoint;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
-@Transactional
+@ExtendWith(MockitoExtension.class)
 class PostServiceTest {
 
-    @Autowired PostService postService;
+    @InjectMocks
+    PostService postService;
 
-    @Test
-    void write() {
-        Post post = new Post();
+    @Mock
+    PostRepository postRepository;
+
+    Post post;
+
+    @BeforeEach
+    void init() {
+        post = new Post();
+        post.setId("axizd");
         post.setTitle("test Title");
         post.setAuthor("test Author");
         post.setContent("set Content");
         post.setLocation(GeoJsonPoint.of(100, 100));
+    }
+
+    @Test
+    void write() {
+        when(postRepository.save(any())).thenReturn(post);
+        when(postRepository.findById(post.getId())).thenReturn(Optional.of(post));
 
         String saveId = postService.write(post);
-
         Post findPost = postService.findOne(saveId).get();
+
+        verify(postRepository).save(any());
+        verify(postRepository).findById(post.getId());
+
         assertThat(post).isEqualTo(findPost);
     }
 }
