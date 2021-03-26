70595300ae5a158d930519169c9ef05b2a671cd6
zxcv859500 <zxcv859500@naver.com>
2021-03-26 17:19:19 +0900

    [BE] refactor: remove redundant package

--- backend/src/main/java/com/bluepa/backend/post/repository/JpaPostRepository.java
+++ backend/src/main/java/com/bluepa/backend/post/repository/JpaPostRepository.java
@@ -2,10 +2,8 @@ package com.bluepa.backend.post.repository;
 
 import com.bluepa.backend.post.domain.Post;
 
-import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface JpaPostRepository extends ElasticsearchRepository<Post, String>, PostRepository {
