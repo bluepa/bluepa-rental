0e89efdc486bcfc78392d9fe26ed183ae563d480
zxcv859500 <zxcv859500@naver.com>
2021-03-26 10:18:45 +0900

    [BE] add: PostService implementation

--- backend/src/main/java/com/bluepa/backend/post/service/PostService.java
+++ backend/src/main/java/com/bluepa/backend/post/service/PostService.java
@@ -1,10 +1,14 @@
 package com.bluepa.backend.post.service;
 
 import com.bluepa.backend.post.domain.Post;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface PostService {
 
     String write(Post post, String cityName);
     Optional<Post> findOne(String id);
+    List<Post> search(String keyword);
 }
