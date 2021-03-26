daf128b6a0c604ebd9f62ebcf471be577c45a313
zxcv859500 <zxcv859500@naver.com>
2021-03-26 15:30:54 +0900

    [BE] add: PostNotFoundException

---
+++ backend/src/main/java/com/bluepa/backend/post/exception/PostNotFoundException.java
@@ -0,0 +1,13 @@
+package com.bluepa.backend.post.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class PostNotFoundException extends RuntimeException {
+
+    public PostNotFoundException(Class<?> entity, String field, String value) {
+        super("글이 존재하지 않습니다. "
+                + "(" + entity.getSimpleName() + ": " + field + "=" + value + ")");
+    }
+}
