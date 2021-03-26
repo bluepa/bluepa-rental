0b6acebdae48c98a9962b7b8787a530cfea33e1a
zxcv859500 <zxcv859500@naver.com>
2021-03-26 14:52:20 +0900

    [BE] test: Error argument change

--- backend/src/test/java/com/bluepa/backend/post/service/PostServiceTest.java
+++ backend/src/test/java/com/bluepa/backend/post/service/PostServiceTest.java
@@ -64,7 +64,7 @@ class PostServiceTest {
     void 검색() {
         when(postRepository.searchPost("test Title", GeoJsonPoint.of(123, 123))).thenReturn(List.of(post));
 
-        List<Post> posts = postRepository.searchPost("test", GeoJsonPoint.of(123, 123));
+        List<Post> posts = postRepository.searchPost("test Title", GeoJsonPoint.of(123, 123));
 
         verify(postRepository).searchPost("test Title", GeoJsonPoint.of(123, 123));
 
