c91fd8059fd7220ded3fe890e494e9127066bf5d
voiciphil <voiciphil@gmail.com>
2021-03-28 02:21:56 +0900

    [BE]test: Mockito를 BDDMockito로 변경

--- backend/src/test/java/com/bluepa/backend/user/service/UserServiceTest.java
+++ backend/src/test/java/com/bluepa/backend/user/service/UserServiceTest.java
@@ -3,8 +3,7 @@ package com.bluepa.backend.user.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.BDDMockito.then;
 
 import com.bluepa.backend.global.security.JwtProvider;
 import com.bluepa.backend.user.domain.EmailAuth;
@@ -65,14 +64,14 @@ public class UserServiceTest {
             .password(password)
             .build();
         EmailAuth emailAuth = new EmailAuth("aaa@gmail.com,123456,true");
-        when(passwordEncoder.encode(any())).thenReturn(password);
-        when(userRepository.save(any())).thenReturn(user);
-        when(emailAuthRepository.findByEmail(email)).thenReturn(Optional.of(emailAuth));
+        given(passwordEncoder.encode(any())).willReturn(password);
+        given(userRepository.save(any())).willReturn(user);
+        given(emailAuthRepository.findByEmail(email)).willReturn(Optional.of(emailAuth));
 
         Long userId = userService.signUp(signUpRequest);
 
-        verify(userRepository).save(any());
-        verify(emailAuthRepository).deleteByEmail(email);
+        then(userRepository).should().save(any());
+        then(emailAuthRepository).should().deleteByEmail(email);
         assertThat(userId).isEqualTo(user.getId());
     }
 
@@ -82,9 +81,9 @@ public class UserServiceTest {
             .email(email)
             .password(password)
             .build();
-        when(passwordEncoder.matches(any(), any())).thenReturn(true);
-        when(userRepository.findByEmail(any())).thenReturn(Optional.of(user));
-        when(provider.createToken(user)).thenReturn("token");
+        given(passwordEncoder.matches(any(), any())).willReturn(true);
+        given(userRepository.findByEmail(any())).willReturn(Optional.of(user));
+        given(provider.createToken(user)).willReturn("token");
 
         String token = userService.signIn(signInRequest);
 
@@ -104,18 +103,18 @@ public class UserServiceTest {
 
     @Test
     void 이메일_전송() {
-        when(userRepository.existsByEmail(email)).thenReturn(false);
+        given(userRepository.existsByEmail(email)).willReturn(false);
 
         userService.sendEmail(email);
 
-        verify(emailAuthRepository).save(any());
-        verify(javaMailSender).send((SimpleMailMessage) any());
+        then(emailAuthRepository).should().save(any());
+        then(javaMailSender).should().send((SimpleMailMessage) any());
     }
 
     @Test
     void 이메일_인증() {
         EmailAuth emailAuth = new EmailAuth(email, 123456);
-        when(emailAuthRepository.findByEmail(email)).thenReturn(Optional.of(emailAuth));
+        given(emailAuthRepository.findByEmail(email)).willReturn(Optional.of(emailAuth));
 
         userService.authenticateEmail(email, 123456);
 
