9ffdbbd08e7e6792b311d409b22baab98084d952
zxcv859500 <guwensoo@gmail.com>
2021-04-02 05:16:06 +0900

    [BE] add: dto cityName for routing

--- backend/src/main/java/com/bluepa/backend/post/dto/SearchRequest.java
+++ backend/src/main/java/com/bluepa/backend/post/dto/SearchRequest.java
@@ -1,11 +1,14 @@
 package com.bluepa.backend.post.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.elasticsearch.core.geo.GeoJsonPoint;
 
 @Getter
+@Builder
 public class SearchRequest {
 
     String keyword;
     GeoJsonPoint location;
+    String cityName;
 }
