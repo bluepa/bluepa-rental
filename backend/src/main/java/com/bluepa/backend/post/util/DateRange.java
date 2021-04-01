9657cc2f738940b6c671c57e9b5ba558e2277024
zxcv859500 <guwensoo@gmail.com>
2021-04-02 05:14:42 +0900

    [BE] add: dto 추가

---
+++ backend/src/main/java/com/bluepa/backend/post/util/DateRange.java
@@ -0,0 +1,13 @@
+package com.bluepa.backend.post.util;
+
+import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DateRange {
+
+    private Date startDate;
+    private Date endDate;
+}
