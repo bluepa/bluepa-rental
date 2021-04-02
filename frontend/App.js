3604c3448e86dd6d71395ed874f176ca7a33870f
YaeheeChoe <yaeheee@outlook.com>
2021-04-02 11:41:58 +0900

    [FE] 기존 컴포넌트 모듈화

--- frontend/App.js
+++ frontend/App.js
@@ -1,22 +1,8 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import LoginPage from './LoginPage';
-import JoinPage from './JoinPage';
-import StartPage from './StartPage';
-
-const Stack = createStackNavigator();
-
+import Login from './navigation/Login'
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Start" component={StartPage} />
-        <Stack.Screen name="Login" component={LoginPage} />
-        <Stack.Screen name="Join" component={JoinPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Login></Login>
   );
 }
 
---
+++ frontend/components/BasicTitle.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default class BasicTitle extends Component {
+    static defaultProps = {
+        width: '100%',
+        height: '18%',
+        text:'untitled',
+    }
+
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <View style={styles.title, {width:this.props.width, height:this.props.height}}>
+                <Text style={styles.text}>{this.props.text}}</Text>
+                <View style={{ width: "100%", borderBottomWidth: 0.5, borderColor: '#444' }} />
+            </View>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    title: {
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize:35,paddingBottom:20,
+    },
+});
