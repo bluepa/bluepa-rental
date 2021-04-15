eda5fe7168e940f3f7f0e9f495e8083dd1ab69f6
YaeheeChoe <yaeheee@outlook.com>
2021-04-15 14:53:00 +0900

    [FE] fix : StyleSheet err

--- frontend/src/navigation/Login.js
+++ frontend/src/navigation/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import {StyleSheet} from 'react-native'
 import EmailJoinScreen from '../pages/Login/EmailJoinScreen'
 import SendEmailScreen from '../pages/Login/SendEmailScreen'
 import StartScreen from '../pages/Login/StartScreen'
